9fc67185e23727ce2a4f76af25056c99c80152be
Dmitry Yarotsky <31436662+dima-yarotsky@users.noreply.github.com>
2022-05-27 17:37:04 +0600

    Добавьте файлы проекта.

---
+++ WebApplication2/Models/CitiesContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models
+{
+  
+        public class CitiesContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+        public CitiesContext(DbContextOptions<CitiesContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+            
+        }
+
+    }
+    }
+
